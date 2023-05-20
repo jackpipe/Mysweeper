296df8698353dbf66a0d826fb844bff7bb6fe64b
Jack <jackbyte@fastmail.fm>
2023-05-20 19:09:58 +0700

    Remove validation tests for board dimensions presence (covered by validator spec0

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Board, type: :model do
   let(:board) { create(:board) }
 
   context "Validations" do
-    [:name, :email, :width, :height, :mines].each do |attr|
+    [:name, :email].each do |attr|
       it { should validate_presence_of(attr) }
     end
   end
