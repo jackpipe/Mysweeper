eb1c4a1668c5f90208694710ac9273d7b23a683c
Jack <jackbyte@fastmail.fm>
2023-05-21 09:57:28 +0700

    Added board model specs to test grid_create

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,19 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Board, type: :model do
-  let(:board) { create(:board) }
+  let(:subject) { build(:board) }
 
   context "Validations" do
     [:name, :email].each do |attr|
       it { should validate_presence_of(attr) }
+      it "should validate the email format"
     end
   end
 
-  context "Grid", focus: true do
+  context "Grid" do
     it "creates a grid" do
-      #g = create_grid
-      #b place_mines( g, mines)
-      #b.compact.count = mines
+      g = subject.create_grid
+      expect(g.count).to eq(subject.width * subject.height)
+    end
+
+    it "creates a single mine" do
+      subject.mines = 1
+      g = subject.create_grid
+      expect(g.count(true)).to eq(1)
+    end
+
+    it "creates full board of mines" do
+      tiles = subject.width * subject.height
+      subject.mines = tiles - 1
+      g = subject.create_grid
+      expect(g.count(true)).to eq(tiles-1)
+    end
+
+    it "creates mines max collision" do
+      tiles = subject.width * subject.height
+      subject.mines = tiles / 2
+      g = subject.create_grid
+      expect(g.count(true)).to eq(tiles/2)
+    end
+
+    it "creates a big grid" do
+      subject = Board.new(width: 1_000, height: 1_000, mines: 500_000)
+      g = subject.create_grid
+      expect(g.count(true)).to eq(500_000)
+      expect(g.count).to eq(1_000_000)
     end
   end
 end
